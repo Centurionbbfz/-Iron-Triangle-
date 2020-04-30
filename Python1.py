db6759127578b3dd7f85d472843df010e7a15112
Centurionbbfz <64575631+Centurionbbfz@users.noreply.github.com>
2020-04-30 14:54:32 +0800

    Add files via upload

---
+++ Python1.py
@@ -0,0 +1,40 @@
+Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+>>> print("Hello World!")
+Hello World!
+>>> print('Hello World!')
+Hello World!
+>>> #用英文的引号，这里单引号等同于双引号，print是打印
+>>> #数学运算符：“+”，“-”，“*”（乘号），“/”（除号）
+>>> 2+2
+4
+>>> 3*4
+12
+>>> (4+8)/2
+6.0
+>>> (-7+2)*(-4)
+20
+>>> #0不能作除数
+>>> #0不能作除数，不信你看↓
+>>> 300/0
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ZeroDivisionError: division by zero
+>>> #这就叫Bug
+>>> 490*0.9
+441.0
+>>> 459*0.9
+413.1
+>>> #浮点数，即小数
+>>> #整数相除会得到浮点数
+>>> 8/2
+4.0
+>>> 6*7.0
+42.0
+>>> 4+1.65
+5.65
+>>> 1+2+3+2.5+4
+12.5
+>>>
